8b38180c2cf4dcef430bd1c35364f98b71474338
TrueRandolf <randolf@yandex.ru>
2026-01-02 17:08:21 +0300

    fix: exclude DB in HomeworkApplication and edit whitelist in WebSecurityConfig

--- src/main/java/ru/skypro/homework/HomeworkApplication.java
+++ src/main/java/ru/skypro/homework/HomeworkApplication.java
@@ -2,9 +2,10 @@ package ru.skypro.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class HomeworkApplication {
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class})
+public class HomeworkApplication  {
   public static void main(String[] args) {
     SpringApplication.run(HomeworkApplication.class, args);
   }
